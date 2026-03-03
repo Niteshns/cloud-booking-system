559d1b0fd5ee19285c1ba407674a876aeb301d1e
Raihan <76593807+RaihanKaskandar@users.noreply.github.com>
2026-03-03 15:48:59 +0100

    Add files via upload
    Het gaat alleen om 1 app.ts file. Geen .env zijn voor deze aangepast

--- payment-service-nodejs/src/app.ts
+++ payment-service-nodejs/src/app.ts
@@ -2,12 +2,23 @@ import amqp from "amqplib";
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import dotenv from "dotenv";
+import mysql from "mysql2/promise";
 
 dotenv.config();
 
 const app = express();
 const port = Number(process.env.PORT ?? 3002);
 const rabbitMqUrl = process.env.RABBITMQ_URL ?? "amqp://localhost:5672";
+
+const databasePool = mysql.createPool({
+  host: "payment-db.ctwgyeke24cg.eu-north-1.rds.amazonaws.com",
+  user: "admin",
+  password: "devopsUVA!",
+  port: 3306,
+  database: "payment_service",
+  waitForConnections: true,
+  connectionLimit: 5,
+});
 
 app.get("/healthz", (_req, res) => {
   res.status(200).json({ status: "ok", service: "payment-service" });
@@ -31,7 +42,7 @@ async function startPaymentWorker() {
 
   await channel.consume(
     queuePaymentRequest,
-    (inputMessage) => {
+    async (inputMessage) => {
       if (!inputMessage) {
         return;
       }
@@ -39,11 +50,32 @@ async function startPaymentWorker() {
       const paymentRequestId = inputMessage.content.toString();
       const paymentConfirmationId = uuidv4();
 
-      const outputMessage = JSON.stringify({ paymentRequestId, paymentConfirmationId });
-      channel.sendToQueue(queuePaymentResponse, Buffer.from(outputMessage));
-      channel.ack(inputMessage);
+      try {
+        const userId = 333;
+        const eventId = 212;
+        const price = 199;
+        const paymentOption = "debit-card";
 
-      console.log("Processed payment request", paymentRequestId);
+        await databasePool.execute(
+          `
+            INSERT INTO payments
+            (user_id, event_id, price, payment_option)
+            VALUES (?, ?, ?, ?)
+          `,
+          [userId, eventId, price, paymentOption]
+        );
+
+        console.log("✅ Payment inserted");
+
+        const outputMessage = JSON.stringify({ paymentRequestId, paymentConfirmationId });
+        channel.sendToQueue(queuePaymentResponse, Buffer.from(outputMessage));
+
+        channel.ack(inputMessage);
+        console.log("Processed payment request", paymentRequestId);
+      } catch (error) {
+        console.error("DB insert failed:", error);
+        channel.nack(inputMessage, false, true);
+      }
     },
     { noAck: false }
   );
