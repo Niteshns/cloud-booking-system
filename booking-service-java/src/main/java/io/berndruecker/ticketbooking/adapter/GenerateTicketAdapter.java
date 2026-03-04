48d4638a695b38b63e38dca3e8a06235b3c6595f
Niteshns <nitesh17ns@gmail.com>
2026-03-04 15:26:38 +0100

    Added async requests | cd for diagrams

--- booking-service-java/src/main/java/io/berndruecker/ticketbooking/adapter/GenerateTicketAdapter.java
+++ booking-service-java/src/main/java/io/berndruecker/ticketbooking/adapter/GenerateTicketAdapter.java
@@ -1,6 +1,7 @@
 package io.berndruecker.ticketbooking.adapter;
 
 import io.berndruecker.ticketbooking.ProcessConstants;
+import io.berndruecker.ticketbooking.rest.BookingResultStore;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import org.slf4j.Logger;
@@ -25,13 +26,22 @@ public class GenerateTicketAdapter {
   @Autowired
   private RestTemplate restTemplate;
 
+  @Autowired
+  private BookingResultStore resultStore;
+
   @JobWorker(type = "generate-ticket")
   public Map<String, Object> callGenerateTicketRestService(final ActivatedJob job) throws IOException {
     logger.info("Generate ticket via REST [" + job + "]");
 
-    if ("ticket".equalsIgnoreCase((String)job.getVariablesAsMap().get(ProcessConstants.VAR_SIMULATE_BOOKING_FAILURE))) {
+    Map<String, Object> vars = job.getVariablesAsMap();
+
+    if ("ticket".equalsIgnoreCase((String) vars.get(ProcessConstants.VAR_SIMULATE_BOOKING_FAILURE))) {
 
       // Simulate a network problem to the HTTP server
+      String bookingRef = (String) vars.get(ProcessConstants.VAR_BOOKING_REFERENCE_ID);
+      if (bookingRef != null) {
+        resultStore.markFailed(bookingRef);
+      }
       throw new IOException("[Simulated] Could not connect to HTTP server");
       
     } else {
@@ -39,6 +49,14 @@ public class GenerateTicketAdapter {
       // Call REST API, simply returns a ticketId
       CreateTicketResponse ticket = restTemplate.getForObject(endpoint, CreateTicketResponse.class);
       logger.info("Succeeded with " + ticket);
+
+      // Store completed result so GET /ticket/{id} can return it
+      String bookingRef = (String) vars.get(ProcessConstants.VAR_BOOKING_REFERENCE_ID);
+      String reservationId = (String) vars.get(ProcessConstants.VAR_RESERVATION_ID);
+      String paymentConfirmationId = (String) vars.get(ProcessConstants.VAR_PAYMENT_CONFIRMATION_ID);
+      if (bookingRef != null) {
+        resultStore.markCompleted(bookingRef, reservationId, paymentConfirmationId, ticket.ticketId);
+      }
 
       return Collections.singletonMap(ProcessConstants.VAR_TICKET_ID, ticket.ticketId);
     }
--- booking-service-java/src/main/java/io/berndruecker/ticketbooking/rest/TicketBookingRestController.java
+++ booking-service-java/src/main/java/io/berndruecker/ticketbooking/rest/TicketBookingRestController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
@@ -16,9 +18,6 @@ import org.springframework.web.server.ServerWebExchange;
 import io.berndruecker.ticketbooking.ProcessConstants;
 import io.berndruecker.ticketbooking.adapter.RetrievePaymentAdapter;
 import io.camunda.zeebe.client.ZeebeClient;
-import io.camunda.zeebe.client.api.ZeebeFuture;
-import io.camunda.zeebe.client.api.command.ClientStatusException;
-import io.camunda.zeebe.client.api.response.ProcessInstanceResult;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 
 @SpringBootConfiguration
@@ -31,54 +30,67 @@ public class TicketBookingRestController {
   @Autowired
   private ZeebeClient client;
 
+  @Autowired
+  private BookingResultStore resultStore;
+
   @PutMapping("/ticket")
   public ResponseEntity<BookTicketResponse> bookTicket(ServerWebExchange exchange) {
     String simulateBookingFailure = exchange.getRequest().getQueryParams().getFirst("simulateBookingFailure");
-    
-    // This would be best generated even in the client to allow idempotency!
+
     BookTicketResponse response = new BookTicketResponse();
     response.bookingReferenceId = UUID.randomUUID().toString();
-    
+    response.status = "processing";
+
     HashMap<String, Object> variables = new HashMap<String, Object>();
     variables.put(ProcessConstants.VAR_BOOKING_REFERENCE_ID, response.bookingReferenceId);
-    if (simulateBookingFailure!=null) {
+    if (simulateBookingFailure != null) {
       variables.put(ProcessConstants.VAR_SIMULATE_BOOKING_FAILURE, simulateBookingFailure);
     }
 
-    // Start new instance of the ticket-booking workflow
-    ZeebeFuture<ProcessInstanceResult> future = client.newCreateInstanceCommand() //
-        .bpmnProcessId("ticket-booking") //
-        .latestVersion() //
-        .variables(variables) //
-        .withResult() // wait for the workflow to finish
-        .send(); // with this we get a future
+    // Track this booking
+    resultStore.markProcessing(response.bookingReferenceId);
 
-    try {
-      // Block until it is really done
-      ProcessInstanceResult workflowInstanceResult = future.join();
+    // Start workflow asynchronously (fire-and-forget, no .withResult())
+    client.newCreateInstanceCommand()
+        .bpmnProcessId("ticket-booking")
+        .latestVersion()
+        .variables(variables)
+        .send();
 
-      // Unwrap data from workflow after it finished
-      response.reservationId = (String) workflowInstanceResult.getVariablesAsMap().get(ProcessConstants.VAR_RESERVATION_ID);
-      response.paymentConfirmationId = (String) workflowInstanceResult.getVariablesAsMap().get(ProcessConstants.VAR_PAYMENT_CONFIRMATION_ID);
-      response.ticketId = (String) workflowInstanceResult.getVariablesAsMap().get(ProcessConstants.VAR_TICKET_ID);
-      
+    logger.info("Started workflow for booking " + response.bookingReferenceId);
+
+    // Return immediately with 202 Accepted
+    return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+  }
+
+  @GetMapping("/ticket/{bookingReferenceId}")
+  public ResponseEntity<BookTicketResponse> getBookingStatus(
+      @PathVariable String bookingReferenceId) {
+
+    BookingResultStore.BookingResult result = resultStore.get(bookingReferenceId);
+    if (result == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    BookTicketResponse response = new BookTicketResponse();
+    response.bookingReferenceId = result.bookingReferenceId;
+    response.status = result.status.name().toLowerCase();
+    response.reservationId = result.reservationId;
+    response.paymentConfirmationId = result.paymentConfirmationId;
+    response.ticketId = result.ticketId;
+
+    if (result.status == BookingResultStore.BookingStatus.COMPLETED) {
       return ResponseEntity.status(HttpStatus.OK).body(response);
-    } catch (ClientStatusException ex) {
-
-      // of course we can run into a timeout if the workflow does not finish
-      // within that timeframe!
-      logger.error("Timeout on waiting for workflow"); //, ex);
-
-      return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    } else {
+      return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
   }
-  
-  // TODO: Add API to query status (if you got an 202 earlier on)
 
   public static class BookTicketResponse {
+    public String status;
     public String reservationId;
     public String paymentConfirmationId;
-    public String ticketId;   
+    public String ticketId;
     public String bookingReferenceId;
 
     public boolean isSuccess() {
